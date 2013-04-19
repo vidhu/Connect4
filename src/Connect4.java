3da7563d2729f34407c6bbb44bc4c65248460b6d
Vidhu Bhatnagar <xx.vidhuxx@gmail.com>
2013-04-18 22:08:23 -0400

    Removed the move method

--- src/Connect4.java
+++ src/Connect4.java
@@ -3,11 +3,7 @@ public class Connect4 {
 
     int[][] board = new int[8][8];
     
-    public static int[][] move(int[][] board, int column, boolean isBlue){
-        
-        return board;
-    }
-    
+
     public static void main(String[] args) {
         
     }
