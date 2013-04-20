da9a9a25e337e971ab0942b09790e4b4894ccd44
Vidhu Bhatnagar <xx.vidhuxx@gmail.com>
2013-04-20 16:16:02 -0400

    Added player class

---
+++ src/Player.java
@@ -0,0 +1,23 @@
+/* Player.java
+ * Name(s): Norberto, Vaemen, Vidhu
+ * 
+ * URL
+ * Purpose: Creates a player class/object for the Connect4.java file. 
+ */
+
+
+public class Player {
+    public  int move(int [][] board, int player) {
+        
+        return 0;
+    }
+    
+    
+    public  int eval(int [][] board) {
+        
+        return 0;
+    }
+    
+    
+}
+
