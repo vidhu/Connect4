c8aef61e2d53f259545e0c53ae3cd8a44968e8e4
Vidhu Bhatnagar <xx.vidhuxx@gmail.com>
2013-04-25 12:44:11 -0400

    Some changes logged in comments

--- src/Connect4.java
+++ src/Connect4.java
@@ -1,40 +1,48 @@
 import java.awt.*;
-import java.net.URL;
 import java.util.EventObject;
 import javax.swing.*;
-import java.util.Random;
+
 
 public class Connect4 {
     //Boord used in this game
-    static int[][] board = new int[8][8]; //[row][column]
-    
     //The grids in the board
     static JButton[][] squares = new JButton[8][8];
+    static int[][] board = new int[8][8];            //game board [row][column]
     
     //GridColors
-    static ImageIcon blankButton = new ImageIcon("src/blank.png");
-    static ImageIcon blueButton = new ImageIcon("src/blue.png");
-    static ImageIcon redButton = new ImageIcon("src/red.png");
+    static ImageIcon blankButton = new ImageIcon("blank.png");
+    static ImageIcon blueButton = new ImageIcon("blue.png");
+    static ImageIcon redButton = new ImageIcon("red.png");
     
-    //A player i.e a bot
-    Player p = new Player("blue");
+    
+    
     
     public static void main(String[] args) throws InterruptedException {
+        Player p = new Player();                   // bot
         drawGui();
+        
+        boolean firstMove = false;
+        
         while(true){
-            makeMove(10, 3);//Bot makes move "makeMove(playerNumber, column)"
-            userTurn();       //user makes move
+            
+            if(firstMove) {                                  //DELTE AFTER TESTING LINE IS ONLY USED TO SIMULATE COMPUTER GOING FIRST
+                makeMove(10, 4);
+                firstMove = false;
+            }                                     
+            
+            userTurn();                                  // user makes move
+            makeMove(10, p.move(board, 10));                 // bot makes move (playerNumber, column)
         }
     }
     
     //returns true is there is a 4 in a row at the latest point where the color was added
     public static boolean isWin(int row, int column){
         return isWinHelper(row, column, 1, -1) || //Check diagonal up 
-                isWinHelper(row, column, 1, 1) || //Check diagonal down
-                isWinHelper(row, column, 1, 0) || //Check horizontal
-                isWinHelper(row, column, 0, 1);   //Check vertical
-
-
+            isWinHelper(row, column, 1, 1) || //Check diagonal down
+            isWinHelper(row, column, 1, 0) || //Check horizontal
+            isWinHelper(row, column, 0, 1);   //Check vertical
+        
+        
     }
     public static boolean isWinHelper(int row, int column, int stepX, int stepY){        
         int playerSum = 0;
@@ -95,12 +103,12 @@ public class Connect4 {
         ImageIcon redButton = new ImageIcon("src/red.png");
         
         //Create Gui Frame
-        JFrame frame = new JFrame("Hash Function Comparison");
-        frame.setSize(500, 500);
+        JFrame frame = new JFrame("Connect 4");
+        frame.setSize(537, 700);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setBackground(new Color(255, 240, 180));
         
-           
+        
         for(int i=0; i<squares.length; i++){
             for(int j=0; j<squares.length; j++){
                 if(board[i][j] == 1){
@@ -110,46 +118,53 @@ public class Connect4 {
                 }else{
                     squares[i][j] = new JButton(blankButton);
                 }
-                JBox.setSize(squares[i][j], 50, 50);
+                JBox.setSize(squares[i][j], 65, 65);
                 squares[i][j].setIconTextGap(0);
             }  
         }
-       
+        
         
         JBox body = JBox.vbox(
-            JBox.hbox(
-                squares[0][0], squares[0][1], squares[0][2], squares[0][3],
-                squares[0][4], squares[0][5], squares[0][6], squares[0][7]
-            ),
-            JBox.hbox(
-                squares[1][0], squares[1][1], squares[1][2], squares[1][3],
-                squares[1][4], squares[1][5], squares[1][6], squares[1][7]
-            ),
-            JBox.hbox(
-                squares[2][0], squares[2][1], squares[2][2], squares[2][3],
-                squares[2][4], squares[2][5], squares[2][6], squares[2][7]
-            ),
-            JBox.hbox(
-                squares[3][0], squares[3][1], squares[3][2], squares[3][3],
-                squares[3][4], squares[3][5], squares[3][6], squares[3][7]
-            ),
-            JBox.hbox(
-                squares[4][0], squares[4][1], squares[4][2], squares[4][3],
-                squares[4][4], squares[4][5], squares[4][6], squares[4][7]
-            ),
-            JBox.hbox(
-                squares[5][0], squares[5][1], squares[5][2], squares[5][3],
-                squares[5][4], squares[5][5], squares[5][6], squares[5][7]
-            ),
-            JBox.hbox(
-                squares[6][0], squares[6][1], squares[6][2], squares[6][3],
-                squares[6][4], squares[6][5], squares[6][6], squares[6][7]
-            ),
-            JBox.hbox(
-                squares[7][0], squares[7][1], squares[7][2], squares[7][3],
-                squares[7][4], squares[7][5], squares[7][6], squares[7][7]
-            )
-        );
+                              JBox.hbox(
+                                        JBox.hglue(),
+                                        JBox.hbox(new JLabel("Connect 4")),
+                                        JBox.hglue()),
+                              JBox.vglue(),
+                              JBox.hbox(
+                                        squares[0][0], squares[0][1], squares[0][2], squares[0][3],
+                                        squares[0][4], squares[0][5], squares[0][6], squares[0][7]
+                                       ),
+                              JBox.hbox(
+                                        squares[1][0], squares[1][1], squares[1][2], squares[1][3],
+                                        squares[1][4], squares[1][5], squares[1][6], squares[1][7]
+                                       ),
+                              JBox.hbox(
+                                        squares[2][0], squares[2][1], squares[2][2], squares[2][3],
+                                        squares[2][4], squares[2][5], squares[2][6], squares[2][7]
+                                       ),
+                              JBox.hbox(
+                                        squares[3][0], squares[3][1], squares[3][2], squares[3][3],
+                                        squares[3][4], squares[3][5], squares[3][6], squares[3][7]
+                                       ),
+                              JBox.hbox(
+                                        squares[4][0], squares[4][1], squares[4][2], squares[4][3],
+                                        squares[4][4], squares[4][5], squares[4][6], squares[4][7]
+                                       ),
+                              JBox.hbox(
+                                        squares[5][0], squares[5][1], squares[5][2], squares[5][3],
+                                        squares[5][4], squares[5][5], squares[5][6], squares[5][7]
+                                       ),
+                              JBox.hbox(
+                                        squares[6][0], squares[6][1], squares[6][2], squares[6][3],
+                                        squares[6][4], squares[6][5], squares[6][6], squares[6][7]
+                                       ),
+                              JBox.hbox(
+                                        squares[7][0], squares[7][1], squares[7][2], squares[7][3],
+                                        squares[7][4], squares[7][5], squares[7][6], squares[7][7]
+                                       ),
+                              JBox.vglue()
+                             );
+        body.setFont(new Font("Connect 4", Font.BOLD, 48));
         
         frame.add(body);
         frame.setVisible(true);
@@ -167,7 +182,7 @@ public class Connect4 {
                 }else{
                     squares[i][j].setIcon(blankButton);
                 }
-                JBox.setSize(squares[i][j], 50, 50);
+                JBox.setSize(squares[i][j], 65, 65);
                 squares[i][j].setIconTextGap(0);
             }  
         }
@@ -189,32 +204,28 @@ public class Connect4 {
             if (name.subSequence(0, 3).equals("box")) {
                 //get the coordinates
                 int column = Integer.parseInt(name.split("[|]+")[2]);
-
+                
                 //Change the board
-                makeMove(10, column);
+                makeMove(1, column);
                 break;
             }
         }
     }
     
-    public static void makeMove(int Player, int column){
-        int row =0;
-        
-        //Sets the next available row
-        while(row<7){
-            row++;
-            if (board[row][column] != 0) {
-                row--;
-                break;
+    public static void makeMove(int Player, int column){                    // Vaeman
+        boolean found = false;
+        int row = board.length-1; 
+        while(!found && row>=0) {
+            if(board[row][column] == 0) {  
+                makeMove(Player, column, row);
+                found = true;
             }
+            row--;
         }
-        
-        makeMove(Player, column, row);
     }
     
     public static void makeMove(int player, int column, int row){
-        board[row][column] = player;
-        
+        board[row][column] = player;        
         //Detect a win
         if (isWin(row, column)) {
             System.out.println("Win detected!");
